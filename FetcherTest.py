d6d9707490d0a6b4fb4a0283cbaedfbe03723b64
https://github.com/17-654-MSE-Analysis/IoT-Group1 <soniag@andrew.cmu.edu>
2017-06-28 11:17:15 -0400

    sample unit test

--- FetcherTest.py
+++ FetcherTest.py
@@ -3,7 +3,7 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
-        self.assertEqual('hello','hello')
+        self.assertEqual('hello','hello1')
 
 if __name__ == '__main__':
     unittest.main()
