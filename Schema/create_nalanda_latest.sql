361b3e060272a40e21b1b4a8c76c661f3952d84f
https://github.com/17-654-MSE-Analysis/IoT-Group1 <soniag@andrew.cmu.edu>
2017-07-19 15:34:02 -0400

    dataabse schema ..name database added

--- Schema/create_nalanda_latest.sql
+++ Schema/create_nalanda_latest.sql
@@ -3,7 +3,8 @@
 -- Host: localhost    Database: nalanda
 -- ------------------------------------------------------
 -- Server version	5.7.17
-
+DROP DATABASE IF EXISTS nalanda;
+CREATE DATABASE nalanda;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
