28761a0d732dfbf78450a6f5338e1d1c7a6c4f50
minghan fu <minghanfu@minghandeMBP.wv.cc.cmu.edu>
2017-07-21 18:06:16 -0400

    add

---
+++ Code/try.py
@@ -0,0 +1,19 @@
+import transformer
+def transformer_plumber():
+        start_date = "2017-07-01 00:00:00"
+        test = transformer.Transformer("mysql+mysqlconnector://root:@localhost/staging","mysql+mysqlconnector://root:@localhost/nalanda1")
+        test.sync_student_info();
+        test.sync_class_info();
+        test.sync_school_info();
+        #test.sync_content()
+        test.clear_log(start_date)
+        test.completed_questions_aggregation_student(start_date)
+        test.correct_questions_aggregation_student(start_date)
+        test.attempted_questions_aggregation_student(start_date)
+        test.completed_student(start_date)
+        test.mastery_level_aggregation_class(start_date)
+        test.mastery_level_aggregation_school(start_date)
+        test.clear_resource()
+
+if __name__ == "__main__":
+        transformer_plumber()
